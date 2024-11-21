990d9a46fc55945c5c1847a6c3a12a96cfb15109
TongZhengHong <zhtong315@gmail.com>
2024-11-21 16:06:46 -0500

    Update keypad to use state machine

--- Lab4/Sources/keyPad.c
+++ Lab4/Sources/keyPad.c
@@ -5,52 +5,45 @@
     Pulse widths: Clock is 24 MHz, period 41 2/3 ns
     Prescale factor 32: Counter tick time is 1 1/3 µs (1/750000)
     Keypad debounce delays for 2 ms
-    Number of cycles needed for 2 ms delay: 0.002 / (1/750000) = 1500
+    Number of cycles needed for 2 ms delay: 0.01 / (1/750000) = 7500
 ----------------------------------------------------------------------- */
-#define KEYPAD_DELAY 1500
+#define TEN_MS 7500
 #define KEYS_RELEASED 0x0F
 
-static volatile unsigned char code = NOKEY;
+char translate(char);
+char getKeyCode(void);
+
+volatile unsigned char keyCode = NOKEY;
 
 void initKeyPad() {
     DDRA = 0b11110000;              // Set to PA4-7 as output and PA0-3 as input
     PUCR = 0b00000001;              // Enable pull-up resistors for Port A (bit 0 PUPAE) 
 
-    TIOS |= 0b00010000;             // Set TC4 to Output Compare
-    TIE  |= 0b00010000;             // Enable interrupt for TC4
+    TIOS |= (1 << 4);               // Set TC4 to Output Compare
+    TIE  |= (1 << 4);               // Enable interrupt for TC4
+
+    TC4 = TCNT + TEN_MS;
+    keyCode = NOKEY;
 }
 
 char pollReadKey() {
-    if (code == NOKEY) {            // ISR has not been called before
-        CFORC |= 0b00010000;        // Force trigger timer 4 interrupt
-        while (code == NOKEY);      // Wait for key result
+    if (keyCode == NOKEY) 
+        return NOKEY;
+    else {
+        char ch = keyCode;
+        keyCode = NOKEY;
+        return ch;
     }
-    
-    char output = code;             // Code should != NOKEY here
-    code = NOKEY;
-    TC4 = TCNT + KEYPAD_DELAY;      // Debounce for 2 ms
-    while (code == NOKEY);          // Wait unit no key has been pressed
-
-    // Check that all keys have been released
-    return (PORTA & KEYS_RELEASED) == KEYS_RELEASED 
-        ? output
-        : NOKEY;
 }
 
 char readKey() {
-    do {
-        PORTA = 0b00000000;             // Enable all rows
-    } while ((PORTA & KEYS_RELEASED) == KEYS_RELEASED);   // Wait for a button press
+    // Wait until a key has been pressed
+    while (keyCode == NOKEY);
 
-    byte prevCode = translate(PORTA);
-    code = NOKEY;
-    TC4 = TCNT + KEYPAD_DELAY;          // Debounce for 2 ms
-    while (code == NOKEY);
-
-    if (prevCode == code) {             // Rising edge debounced
-        return pollReadKey();
-    }
-    return NOKEY;
+    // Obtain code and reset keyCode
+    char ch = keyCode;
+    keyCode = NOKEY;
+    return ch;
 }
 
 char translate(char portA) {
@@ -92,12 +85,56 @@ char translate(char portA) {
     }
 }
 
-void interrupt VectorNumber_Vtimch4 tc4_isr(void) {
+char getKeyCode() {
     for (int i = 0; i < 4; i++) {
-        byte outputMask = ~(1 << i);    // Invert due to pull up resistors
-        PORTA |= outputMask << 4;       // Shift to upper 4 bits for output
+        PORTA |=  ~(1 << (i + 4));      // Shift to upper 4 bits for output pins
         code = translate(PORTA);
 
         if (code != BADCODE) break;     // Terminate if valid code is obtained
     }
+}
+
+#define WAIT_PRESS          1
+#define DEBOUNCE_PRESS      2
+#define WAIT_RELEASE        3
+#define DEBOUNCE_RELEASE    4
+
+void interrupt VectorNumber_Vtimch4 tc4_isr(void) {
+    static byte state = WAIT_PRESS;
+    static byte prevCode;
+
+    switch (state) {
+    case WAIT_PRESS:
+        prevCode = PORTA;
+        // Move to debounce if any key has been pressed
+        if (PORTA & KEYS_RELEASED != KEYS_RELEASED)
+            state = DEBOUNCE_PRESS;
+        break;
+
+    case DEBOUNCE_PRESS:
+        if (PORTA == prevCode) {
+            prevCode = getKeyCode();
+            state = WAIT_RELEASE; 
+
+        } else state = WAIT_PRESS;
+        break;
+
+    case WAIT_RELEASE:
+        if (PORTA == KEYS_RELEASED) 
+            state = DEBOUNCE_RELEASE;
+        break;
+
+    case DEBOUNCE_RELEASE:
+        if (PORTA == KEYS_RELEASED) {
+            keyCode = prevCode;
+            state = WAIT_PRESS
+
+        } else state = WAIT_RELEASE;
+        break;
+    
+    default:
+        break;
+    }
+
+    TC4 += TEN_MS; 
 }
